18aa97ead85b88b24429abd1516836e8e5e875f3
Ishola Sodiq Olatunde <ishola.olatundey@gmail.com>
2025-10-01 20:33:25 +0100

    updates

--- myApp.js
+++ myApp.js
@@ -4,12 +4,12 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
+   let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
 });
-
-
 
 
 
