118000013f6c2513452756793bdba9df5815c8b0
Ishola Sodiq Olatunde <ishola.olatundey@gmail.com>
2025-10-01 19:43:45 +0100

    updates

--- myApp.js
+++ myApp.js
@@ -3,10 +3,10 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-
-res.sendFile(__dirname + "/views/index.html")
-res.json({message : 'Hello Jason'})
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
 });
 
 
