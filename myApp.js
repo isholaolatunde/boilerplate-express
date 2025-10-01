f9344c2f7935395690768ef192398f2376944885
Ishola Sodiq Olatunde <ishola.olatundey@gmail.com>
2025-10-01 13:09:30 +0100

    upddates

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get("/", function(req, res) {
+app.use("/public", express.static(__dirname + "/public/style.css"))
 res.sendFile(__dirname + "/views/index.html")
 });
 
